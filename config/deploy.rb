5500d5683d52dba56e7a1f9bdc4d84082d930565
Sullivan SENECHAL <sullivan@nexylan.com>
2014-05-06 16:03:29 +0200

    Set logger level to info.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,11 @@
-# config valid only for Capistrano 3.1
-lock '3.2.1'
-
 set :application, 'capistrano-php-sandbox'
 set :repo_url, 'git@github.com:Nexylan/capistrano-php-sandbox.git'
 
 set :deploy_to, '/var/www/php-sandbox.cap/html'
 
 set :scm, :git
+
+set :log_level, Logger::INFO
 
 set :linked_files, %w{config/config.inc.php}
 
